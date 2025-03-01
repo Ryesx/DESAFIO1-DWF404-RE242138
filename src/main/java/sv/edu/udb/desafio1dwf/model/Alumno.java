07f76fca22a69ad9f90721b6676bc8101eb5b3af
reyes <jose.eche777@gmail.com>
2025-02-28 21:18:26 -0600

    primeros cambios del desafio1

---
+++ src/main/java/sv/edu/udb/desafio1dwf/model/Alumno.java
@@ -0,0 +1,4 @@
+package sv.edu.udb.desafio1dwf.model;
+
+public class Alumno {
+}
---
+++ src/test/java/sv/edu/udb/desafio1dwf/Desafio1DwfApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.edu.udb.desafio1dwf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio1DwfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
